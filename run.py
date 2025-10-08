47b442ed102973a9e10c760c69ef890d606090e4
CamelinF <francoisdu44@live.fr>
2025-10-08 10:07:13 -0400

    Test

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 
 data=["hepatitis","chess","mushroom","retail","splice1","eisen","pumsb","pumsb_star","connect","weatherAUS","twitter"]
-
+data=["hepatitis"]
 for i in data:
     print(i)
     # nombre de transactions C=10,  nombre d'itération n=25, Fréquence dans les transactions échantillonné f= 0.2 , nombre de motifs à présenter k=5
